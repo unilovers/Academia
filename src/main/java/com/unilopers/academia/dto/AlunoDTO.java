60e7247797e35cf57a2018a2ae80bb25564f617a
GiovaneBonache <160802024+GiovaneBonache@users.noreply.github.com>
2025-11-26 21:41:31 -0300

    ✨ Criação do DTO de entrada AlunoDTO

---
+++ src/main/java/com/unilopers/academia/dto/AlunoDTO.java
@@ -0,0 +1,74 @@
+package com.unilopers.academia.dto;
+
+import java.time.LocalDate;
+
+public class AlunoDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private Boolean ativo;
+
+
+    public AlunoDTO(Long id, String nome, String email, String telefone, LocalDate dataNascimento, Boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.ativo = ativo;
+    }
+
+    public AlunoDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+
+}
