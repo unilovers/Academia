8c7f263b2d5b390d316fde469109d896ca23c984
Ramon <ramoncfigueiredo@hotmail.com>
2025-11-26 23:16:32 -0300

    🏗️ Criação do repositório ItemMatriculaRepository

---
+++ src/main/java/com/unilopers/academia/repository/ItemMatriculaRepository.java
@@ -0,0 +1,7 @@
+package com.unilopers.academia.repository;
+
+import com.unilopers.academia.model.ItemMatricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemMatriculaRepository extends JpaRepository<ItemMatricula, Long> {
+}
