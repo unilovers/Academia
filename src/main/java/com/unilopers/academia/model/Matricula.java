02e6fb37d3d3ae557e3d55c8c5a43b264c30feec
Murilo Cortez <163426076+MuriloCortez100@users.noreply.github.com>
2025-11-26 23:09:03 -0300

    🗃️ Criação da entidade Matricula

---
+++ src/main/java/com/unilopers/academia/model/Matricula.java
@@ -0,0 +1,86 @@
+package com.unilopers.academia.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "matriculas")
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "plano_id")
+    private Plano plano;
+
+    @Column(name = "data_inicio", nullable = false)
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    private LocalDate dataFim;
+
+    @Column(name = "ativa", nullable = false)
+    private Boolean ativa = true;
+
+
+    public Matricula(Aluno aluno, Plano plano, LocalDate dataInicio, LocalDate dataFim) {
+        this.aluno = aluno; this.plano = plano; this.dataInicio = dataInicio; this.dataFim = dataFim; this.ativa = true;
+    }
+
+    public Matricula() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Plano getPlano() {
+        return plano;
+    }
+
+    public void setPlano(Plano plano) {
+        this.plano = plano;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(Boolean ativa) {
+        this.ativa = ativa;
+    }
+
+}
